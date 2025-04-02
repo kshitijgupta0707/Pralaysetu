db70386e6a1f0a1d439edc14bf44c57d24e63292
= <=>
2025-04-02 12:57:45 +0530

    initializing frontedn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="App">
+        
+        <header className="App-header">
+              <h1>p</h1>
+              <p>
+                This is a simple React application.
+              </p>
+        </header>
+        <main>
+              <p>
+                
+              </p>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
