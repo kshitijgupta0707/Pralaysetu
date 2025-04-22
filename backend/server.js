bea9a79db223c94e683083b5dab39951e7dba685
Nandini Jindal <nandinijindal010@gmail.com>
2025-04-22 21:18:43 +0530

    server code

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,50 @@ app.use("/api/admin", adminRoutes);
 //     res.status(200).json({key: process.env.RAZORPAY_API_KEY});
 // })
 
+app.post("/api/predict_earthquake", async (req, res) => {
+  try {
+      const response = await axios.post('http://localhost:5001/api/predict_earthquake', req.body);
+      res.json(response.data);
+  } catch (error) {
+      res.status(500).json({ error: "Error in prediction" });
+  }
+});
+
+// Route to handle flood prediction
+app.post("/api/predict_flood", async (req, res) => {
+  try {
+    const modifiedRequestBody = {
+      ...req.body,
+      MonsoonIntensity: 4.987700,
+      TopographyDrainage: 4.987400,
+      RiverManagement: 5.007850,
+      Deforestation: 5.010550,
+      Urbanization: 4.985425,
+      ClimateChange: 4.983300,
+      DamsQuality: 5.019000,
+      Siltation: 5.000300,
+      AgriculturalPractices: 5.004300,
+      Encroachments: 4.992725,
+      IneffectiveDisasterPreparedness: 5.002725,
+      DrainageSystems: 5.001850,
+      CoastalVulnerability: 4.987075,
+      Watersheds: 4.978125,
+      DeterioratingInfrastructure: 4.986325,
+      PopulationScore: 4.991500,
+      WetlandLoss: 5.002450,
+      InadequatePlanning: 5.003375,
+      PoliticalFactors: 4.992100
+    };
+
+    // TopographyDrainage, RiverManagement, DamsQuality, Encroachments, Landslides, DeterioratingInfrastructure, PopulationScore
+
+    const response = await axios.post('http://localhost:5001/api/predict_flood', modifiedRequestBody);
+    res.json(response.data);
+  } catch (error) {
+      res.status(500).json({ error: "Error in prediction" });
+  }
+});
+
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
