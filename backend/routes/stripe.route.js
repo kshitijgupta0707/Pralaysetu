353c55d32cd25165b9cbaa1c7598f46a4af12120
= <kshitijgupta070704@gmail.com>
2025-04-24 01:23:36 +0530

    added

--- backend/routes/stripe.route.js
+++ backend/routes/stripe.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 import { stripeWebhookHandler } from "../controllers/stripe.controller.js";
 import { createCheckoutSession } from "../controllers/stripe.controller.js";
 router.post('/create-checkout-session', createCheckoutSession);
+
+// When using stripe.webhooks.constructEvent, Stripe needs the raw request body, not the parsed JSON.
 router.post("/webhook", express.raw({ type: "application/json" }), stripeWebhookHandler);
 
 
