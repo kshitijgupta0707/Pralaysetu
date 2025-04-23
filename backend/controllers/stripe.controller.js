7a16eef0786186a8f7f5eac641a61770e5de4d15
= <kshitijgupta070704@gmail.com>
2025-04-24 01:21:59 +0530

    Corrected the error in webhook

--- backend/controllers/stripe.controller.js
+++ backend/controllers/stripe.controller.js
@@ -6,14 +6,14 @@ dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16", // or latest supported version
 });
- const createCheckoutSession = async (req, res) => {
-  const { amount , ngoId , donorEmail , fundraiserId } = req.body;
-  if(!amount || !ngoId || !donorEmail || !fundraiserId) {
-    return res.status(400).json({ error: "Missing required fields" });    
+const createCheckoutSession = async (req, res) => {
+  const { amount, ngoId, donorEmail, fundraiserId } = req.body;
+  if (!amount || !ngoId || !donorEmail || !fundraiserId) {
+    return res.status(400).json({ error: "Missing required fields" });
   }
-console.log("Amount received:", amount);
+  console.log("Amount received:", amount);
   try {
-    
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: [{
@@ -27,8 +27,8 @@ console.log("Amount received:", amount);
         quantity: 1,
       }],
       mode: 'payment',
-      success_url: 'http://localhost:5173/success',
-      cancel_url: 'http://localhost:5173/cancel',
+      success_url: 'https://pralaysetu.vercel.app/success',
+      cancel_url: 'https://pralaysetu.vercel.app/cancel',
       customer_email: donorEmail, // Optional: pre-fill email
       metadata: {
         ngoId, // Pass NGO ID for later use in webhook
@@ -41,7 +41,7 @@ console.log("Amount received:", amount);
 
 
     // console.log("Stripe session created:", session);
-    console.log("Send the data to the frontend about the payment" )
+    console.log("Send the data to the frontend about the payment")
     res.json({ id: session.id });
   } catch (error) {
     console.error('Stripe Error:', error.message);
@@ -56,23 +56,25 @@ console.log("Amount received:", amount);
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-const stripeWebhookHandler = async(req, res) => {
+const stripeWebhookHandler = async (req, res) => {
+  console.log("REquest from stripe server", req.body)
+  console.log("Webhook received:", req.body);
   const sig = req.headers["stripe-signature"];
   let event;
-  
+
   try {
     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
   } catch (err) {
     console.error("Webhook error:", err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
-  
+
   if (event.type === "checkout.session.completed") {
     const session = event.data.object;
-    
+
     // Example metadata you added during checkout
-    const { ngoId , fundraiserId } = session.metadata;
-    
+    const { ngoId, fundraiserId } = session.metadata;
+
     // Save donation in DB
     await Donation.create({
       ngoId,
@@ -83,13 +85,13 @@ const stripeWebhookHandler = async(req, res) => {
     });
     //update the fundraiser amount
     const fundraiser = await Fundraiser.findById(fundraiserId);
-    fundraiser.raisedAmount += session.amount_total / 100; 
+    fundraiser.raisedAmount += session.amount_total / 100;
     await fundraiser.save();
     console.log("Fundraiser updated:", fundraiser);
-    
+
     console.log("✅ Donation recorded:", session.id);
   }
 
   res.status(200).send("Received");
 }
-  export {createCheckoutSession , stripeWebhookHandler}
+export { createCheckoutSession, stripeWebhookHandler }
