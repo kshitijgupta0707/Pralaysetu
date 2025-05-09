09b52c5086a332a7df367eddb59c235b050327b3
= <kshitijgupta070704@gmail.com>
2025-05-10 00:06:36 +0530

    done some changes in notification save

--- frontend/src/utils/RequestPermission.js
+++ frontend/src/utils/RequestPermission.js
@@ -14,38 +14,57 @@ export const requestNotificationPermission = async (authUser) => {
     const permission = await Notification.requestPermission();
 
     if (permission === "granted") {
-      let fcmToken = localStorage.getItem("fcmToken");
-      if (!fcmToken) {
-        fcmToken = await getToken(messaging, {
+        let fcmToken = await getToken(messaging, {
           vapidKey: import.meta.env.VITE_VAPID_KEY,
         });
-
-        if (fcmToken) {
-          localStorage.setItem("fcmToken", fcmToken);
-        }
-      }
-      console.log("Permission granted");
-      if (fcmToken) {
-
-        console.log("Generated the FCM Token:", fcmToken);
-
-        if (authUser?._id) {
           await axiosInstance.post("/notification/save-token", {
             userId: authUser._id,
             token: fcmToken,
           });
-
-          console.log("🎯 Token saved to DB");
-        } else {
-          console.warn("⚠️ No authenticated user found");
-        }
-      } else {
-        console.warn("⚠️ Failed to get FCM token");
-      }
-    } else {
-      console.warn("❌ Notification permission denied");
     }
   } catch (error) {
     console.error("❌ Error getting permission/token:", error);
   }
 };
+// export const requestNotificationPermission = async (authUser) => {
+//   try {
+    
+//     console.log("At request notification function");
+//     const permission = await Notification.requestPermission();
+
+//     if (permission === "granted") {
+//       let fcmToken = localStorage.getItem("fcmToken");
+//       if (!fcmToken) {
+//         fcmToken = await getToken(messaging, {
+//           vapidKey: import.meta.env.VITE_VAPID_KEY,
+//         });
+
+//         if (fcmToken) {
+//           localStorage.setItem("fcmToken", fcmToken);
+//         }
+//       }
+//       console.log("Permission granted");
+//       if (fcmToken) {
+
+//         console.log("Generated the FCM Token:", fcmToken);
+
+//         if (authUser?._id) {
+//           await axiosInstance.post("/notification/save-token", {
+//             userId: authUser._id,
+//             token: fcmToken,
+//           });
+
+//           console.log("🎯 Token saved to DB");
+//         } else {
+//           console.warn("⚠️ No authenticated user found");
+//         }
+//       } else {
+//         console.warn("⚠️ Failed to get FCM token");
+//       }
+//     } else {
+//       console.warn("❌ Notification permission denied");
+//     }
+//   } catch (error) {
+//     console.error("❌ Error getting permission/token:", error);
+//   }
+// };
